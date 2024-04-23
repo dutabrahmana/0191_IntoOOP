cf6ac515129047da7de8b38f1533fde0d03c619c
dutabrahmana <dutalinggau25@gmail.com>
2024-04-23 09:40:20 +0700

    membuat awalan include iostream

--- Praktikum pemdas/Praktikum pemdas.cpp
+++ Praktikum pemdas/Praktikum pemdas.cpp
@@ -1,53 +1,9 @@
 #include <iostream>
-using namespace std;
 
-class mahasiswa {
-public:
-	string nama;
-	int umur;
-	string jurusan;
 
-	void output() {
-		cout << "Nama = " << nama << endl;
-		cout << "Umur = " << umur << endl;
-		cout << "jurusan = " << jurusan << endl;
+
 	}
 };
 
-class MataKuliah {
-private:
-	string kodeMK;
-	string namaMK;
-	int sks;
-public:
-	void input() {
-		cout << "kode MK =";
-		cin >> kodeMK;
-		cout << "Nama MK =";
-		cin >> namaMK;
-		cout << "SKS =";
-		cin >> sks;
-	}
-
-	void output() {
-		cout << "kode MK = " << kodeMK << endl;
-		cout << "Nama MK = " << namaMK << endl;
-		cout << "SKS : " << sks << endl;
-	}
-};
-
-int main() {
-	mahasiswa mhs;
-	MataKuliah mk;
-
-	mhs.nama = "Duta Brahmana Nusantara";
-	mhs.umur = 17;
-	mhs.jurusan = "Teknologi Informasi";
-	mhs.output();
-
-	mk.input();
-	mk.output();
-
-	return 0;
 
 }
